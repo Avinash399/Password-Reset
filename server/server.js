7515d31e905ab176b76e0aa7cc33105ec810b35c
Avinash399 <sonugupta69121@gmail.com>
2025-09-13 13:37:48 +0530

    Fix Backend Url

--- server/server.js
+++ server/server.js
@@ -16,17 +16,12 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: true,
+  origin: [
+    "https://password-reset-seven.vercel.app"
+  ],
+  methods: ["GET", "POST"],
   credentials: true
-}));
-
-// app.use(cors({
-//   origin: [
-//     "https://password-reset-seven.vercel.app"
-//   ],
-//   methods: ["GET", "POST","PUT"],
-//   credentials: true
-// }))
+}))
 
 app.get('/', (req, res) => {
   return res.status(200).json({success: true, message: "Backend running"})
